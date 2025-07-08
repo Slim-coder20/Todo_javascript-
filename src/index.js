21332c7ef35f429cce49b08e99315b8eb4ca0954
slim <slimabida21@gmail.com>
2025-07-08 20:35:15 +0200

    modifier le statut de todo

--- src/index.js
+++ src/index.js
@@ -1,26 +1,19 @@
 import "./style.css";
-
 /* 1- Affichage des Todos : on créé une référence a notre liste non ordonnée en utilisant 
 la méthode querySelector et en passant le type d'elément 
 */
-const ul = document.querySelector('ul'); 
+
+const ul = document.querySelector("ul");
 
 /* 2- Ajout de Todo : Nous commençon par créé les deux références dont on aura besoin le form et l'input via le querySelector  */
-const form = document.querySelector('form');
-const input = document.querySelector('form > input'); 
 
-/* Nous allons ensuite créer un écouteur et le gestionnaire d'événement correspondant :  */ 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const value = input.value;
-  input.value = ''; 
-  addTodo(value);
-  displayTodo();
-}); 
+const form = document.querySelector("form");
+const input = document.querySelector("form > input");
+
 /*nous ajoutons un tableau de todos de départ  */ 
 const todos = [
   {
-    text: "Je suis une todo",
+    text: "je suis une todo",
     done: false,
   },
   {
@@ -29,42 +22,59 @@ const todos = [
   },
 ];
 
-/* Ensuite nous créons une fonctions qui permet d'afficher nos todos initiales */ 
+/* Nous allons ensuite créer un écouteur et le gestionnaire d'événement correspondant :  */ 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const value = input.value;
+  input.value = "";
+  addTodo(value);
+});
+
 const displayTodo = () => {
   const todosNode = todos.map((todo, index) => {
-    return createTodoElement(todo, index); 
-  })
-  ul.innerHTML = ''; 
-  ul.append(...todosNode); 
- };
-/* Nous créons enfin la fonction qui va créer une élément HTML pour chaque objet todo de notre tableau de todos :*/
-const createTodoElement = (todo, index) => {
-  const li = document.createElement('li');
-  const buttonDelete = document.createElement('button');
-  buttonDelete.addEventListener('click', (event) =>{
-    deleteTodo(index); 
+    return createTodoElement(todo, index);
   });
-  buttonDelete.innerHTML = `Supprimer`; 
-  li.innerHTML = `
-    <span class="todo ${ todo.done ? 'done' : ''}"></span>
-    <p>${ todo.text}</p>
-  `;
-  li.appendChild(buttonDelete); 
-  return li; 
+  ul.innerHTML = "";
+  ul.append(...todosNode);
+};
 
-}
-/* Nous créons une fonction addTodo qui va reçevoir comme argument le text de la todo 
-qui utilisera la méthode push pour ajouter une nouvelle Todo */ 
+const createTodoElement = (todo, index) => {
+  const li = document.createElement("li");
+  const buttonDelete = document.createElement("button");
+  buttonDelete.innerHTML = "Supprimer";
+  buttonDelete.addEventListener("click", (event) => {
+    event.stopPropagation();
+    deleteTodo(index);
+  });
+  li.innerHTML = `
+    <span class="todo ${todo.done ? "done" : ""}"></span>
+    <p>${todo.text}</p>
+  `;
+  li.append(buttonDelete);
+  li.addEventListener("click", (event) => {
+    toggleTodo(index);
+  });
+  return li;
+};
+
+/* création d'une fonction pour ajouter des Todos  */ 
 const addTodo = (text) => {
   todos.push({
-    text, 
-    done: false 
-  })
+    text,
+    done: false,
+  });
+  displayTodo();
+};
 
-}
-
+/* Création d'une fonction pour supprimer une Todo */
 const deleteTodo = (index) => {
   todos.splice(index, 1);
-  displayTodo(); 
-}
-displayTodo(); 
+  displayTodo();
+};
+/* Création d'une fonction pour modifier le statut d'une Todo */
+const toggleTodo = (index) => {
+  todos[index].done = !todos[index].done;
+  displayTodo();
+};
+
+displayTodo();
