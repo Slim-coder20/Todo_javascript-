f67fb96ce043a59a7a64930bdc181297d1432c2a
slim <slimabida21@gmail.com>
2025-07-08 09:38:20 +0200

    Affichage des todos OK

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 import "./style.css";
+
+const ul = document.querySelector('ul'); 
+
+const todos = [
+  {
+    text: "Je suis une todo",
+    done: false,
+  },
+  {
+    text: "faire du javascript",
+    done: true,
+  },
+];
+
+// On va créé une fonction qui va prendre en paramettre notre tableau de todo et l'afficher sur le DOM pour l'utilisateur //
+const displayTodo = () => {
+  const todosNode = todos.map((todo, index) => {
+    return createTodoElement(todo, index); 
+  })
+  ul.innerHTML = ''; 
+  ul.append(...todosNode); 
+ };
+
+const createTodoElement = (todo, index) => {
+  const li = document.createElement('li');
+  li.innerHTML = `
+    <span class="todo ${ todo.done ? 'done' : ''}"></span>
+    <p>${ todo.text}</p>
+    <button>Supprimer</button>
+  
+  `;
+  return li; 
+
+}
+displayTodo(); 
